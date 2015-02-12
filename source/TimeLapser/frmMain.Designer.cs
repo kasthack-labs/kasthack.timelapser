546a46a661768ab5424c76cc102bca8b8877b91a
kasthack <kasthack@epicm.org>
2015-02-12 13:27:15 +0300

    autosplit, icon, minimization to tray, default button, browse folder button

--- source/TimeLapser/frmMain.Designer.cs
+++ source/TimeLapser/frmMain.Designer.cs
@@ -15,7 +15,7 @@
         {
             if (disposing && (components != null))
             {
-                components.Dispose();
+                components.Dispose(); nicon.Dispose();
             }
             base.Dispose(disposing);
         }
@@ -28,6 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
             this.txtPath = new System.Windows.Forms.TextBox();
             this.lblPath = new System.Windows.Forms.Label();
             this.lblFormat = new System.Windows.Forms.Label();
@@ -42,22 +44,28 @@
             this.budBitrate = new System.Windows.Forms.NumericUpDown();
             this.label3 = new System.Windows.Forms.Label();
             this.nudFramerate = new System.Windows.Forms.NumericUpDown();
+            this.btnbrs = new System.Windows.Forms.Button();
+            this.fbdSave = new System.Windows.Forms.FolderBrowserDialog();
+            this.chkSplit = new System.Windows.Forms.CheckBox();
+            this.nudSplitInterval = new System.Windows.Forms.NumericUpDown();
+            this.nicon = new System.Windows.Forms.NotifyIcon(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.nudFreq)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.budBitrate)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.nudFramerate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSplitInterval)).BeginInit();
             this.SuspendLayout();
             // 
             // txtPath
             // 
-            this.txtPath.Location = new System.Drawing.Point(10, 30);
+            this.txtPath.Location = new System.Drawing.Point(12, 30);
             this.txtPath.Name = "txtPath";
-            this.txtPath.Size = new System.Drawing.Size(268, 20);
+            this.txtPath.Size = new System.Drawing.Size(241, 20);
             this.txtPath.TabIndex = 0;
             // 
             // lblPath
             // 
             this.lblPath.AutoSize = true;
-            this.lblPath.Location = new System.Drawing.Point(10, 13);
+            this.lblPath.Location = new System.Drawing.Point(12, 13);
             this.lblPath.Name = "lblPath";
             this.lblPath.Size = new System.Drawing.Size(63, 13);
             this.lblPath.TabIndex = 1;
@@ -66,7 +74,7 @@
             // lblFormat
             // 
             this.lblFormat.AutoSize = true;
-            this.lblFormat.Location = new System.Drawing.Point(10, 57);
+            this.lblFormat.Location = new System.Drawing.Point(9, 97);
             this.lblFormat.Name = "lblFormat";
             this.lblFormat.Size = new System.Drawing.Size(39, 13);
             this.lblFormat.TabIndex = 2;
@@ -76,23 +84,23 @@
             // 
             this.cmbFormat.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbFormat.FormattingEnabled = true;
-            this.cmbFormat.Location = new System.Drawing.Point(10, 74);
+            this.cmbFormat.Location = new System.Drawing.Point(70, 94);
             this.cmbFormat.Name = "cmbFormat";
-            this.cmbFormat.Size = new System.Drawing.Size(97, 21);
+            this.cmbFormat.Size = new System.Drawing.Size(56, 21);
             this.cmbFormat.TabIndex = 1;
             // 
             // lblFreq
             // 
             this.lblFreq.AutoSize = true;
-            this.lblFreq.Location = new System.Drawing.Point(10, 102);
+            this.lblFreq.Location = new System.Drawing.Point(9, 61);
             this.lblFreq.Name = "lblFreq";
-            this.lblFreq.Size = new System.Drawing.Size(42, 13);
+            this.lblFreq.Size = new System.Drawing.Size(61, 13);
             this.lblFreq.TabIndex = 4;
-            this.lblFreq.Text = "Interval";
+            this.lblFreq.Text = "Interval(ms)";
             // 
             // nudFreq
             // 
-            this.nudFreq.Location = new System.Drawing.Point(10, 131);
+            this.nudFreq.Location = new System.Drawing.Point(70, 61);
             this.nudFreq.Maximum = new decimal(new int[] {
             100000,
             0,
@@ -104,7 +112,7 @@
             0,
             0});
             this.nudFreq.Name = "nudFreq";
-            this.nudFreq.Size = new System.Drawing.Size(97, 20);
+            this.nudFreq.Size = new System.Drawing.Size(56, 20);
             this.nudFreq.TabIndex = 3;
             this.nudFreq.Value = new decimal(new int[] {
             500,
@@ -114,9 +122,9 @@
             // 
             // btnGo
             // 
-            this.btnGo.Location = new System.Drawing.Point(191, 235);
+            this.btnGo.Location = new System.Drawing.Point(211, 213);
             this.btnGo.Name = "btnGo";
-            this.btnGo.Size = new System.Drawing.Size(87, 23);
+            this.btnGo.Size = new System.Drawing.Size(67, 23);
             this.btnGo.TabIndex = 6;
             this.btnGo.Text = "Go";
             this.btnGo.UseVisualStyleBackColor = true;
@@ -125,7 +133,7 @@
             // lblTime
             // 
             this.lblTime.AutoSize = true;
-            this.lblTime.Location = new System.Drawing.Point(10, 235);
+            this.lblTime.Location = new System.Drawing.Point(12, 213);
             this.lblTime.Name = "lblTime";
             this.lblTime.Size = new System.Drawing.Size(46, 13);
             this.lblTime.TabIndex = 7;
@@ -134,7 +142,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(10, 172);
+            this.label1.Location = new System.Drawing.Point(9, 133);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(41, 13);
             this.label1.TabIndex = 8;
@@ -144,33 +152,38 @@
             // 
             this.cmbScreen.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbScreen.FormattingEnabled = true;
-            this.cmbScreen.Location = new System.Drawing.Point(10, 197);
+            this.cmbScreen.Location = new System.Drawing.Point(70, 133);
             this.cmbScreen.Name = "cmbScreen";
-            this.cmbScreen.Size = new System.Drawing.Size(268, 21);
+            this.cmbScreen.Size = new System.Drawing.Size(208, 21);
             this.cmbScreen.TabIndex = 5;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(139, 58);
+            this.label2.Location = new System.Drawing.Point(132, 97);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.Size = new System.Drawing.Size(68, 13);
             this.label2.TabIndex = 10;
-            this.label2.Text = "Bitrate(kbps)";
+            this.label2.Text = "Bitrate(mbps)";
             // 
             // budBitrate
             // 
-            this.budBitrate.Location = new System.Drawing.Point(142, 74);
+            this.budBitrate.Location = new System.Drawing.Point(211, 97);
             this.budBitrate.Maximum = new decimal(new int[] {
-            100000,
+            300,
+            0,
+            0,
+            0});
+            this.budBitrate.Minimum = new decimal(new int[] {
+            1,
             0,
             0,
             0});
             this.budBitrate.Name = "budBitrate";
-            this.budBitrate.Size = new System.Drawing.Size(136, 20);
+            this.budBitrate.Size = new System.Drawing.Size(67, 20);
             this.budBitrate.TabIndex = 2;
             this.budBitrate.Value = new decimal(new int[] {
-            30000,
+            30,
             0,
             0,
             0});
@@ -178,7 +191,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(139, 102);
+            this.label3.Location = new System.Drawing.Point(132, 61);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(54, 13);
             this.label3.TabIndex = 12;
@@ -186,14 +199,14 @@
             // 
             // nudFramerate
             // 
-            this.nudFramerate.Location = new System.Drawing.Point(142, 131);
+            this.nudFramerate.Location = new System.Drawing.Point(211, 61);
             this.nudFramerate.Minimum = new decimal(new int[] {
             1,
             0,
             0,
             0});
             this.nudFramerate.Name = "nudFramerate";
-            this.nudFramerate.Size = new System.Drawing.Size(136, 20);
+            this.nudFramerate.Size = new System.Drawing.Size(67, 20);
             this.nudFramerate.TabIndex = 4;
             this.nudFramerate.Value = new decimal(new int[] {
             30,
@@ -201,11 +214,65 @@
             0,
             0});
             // 
+            // btnbrs
+            // 
+            this.btnbrs.Location = new System.Drawing.Point(257, 30);
+            this.btnbrs.Name = "btnbrs";
+            this.btnbrs.Size = new System.Drawing.Size(21, 20);
+            this.btnbrs.TabIndex = 13;
+            this.btnbrs.Text = "...";
+            this.btnbrs.UseVisualStyleBackColor = true;
+            this.btnbrs.Click += new System.EventHandler(this.btnbrs_Click);
+            // 
+            // chkSplit
+            // 
+            this.chkSplit.AutoSize = true;
+            this.chkSplit.Location = new System.Drawing.Point(12, 169);
+            this.chkSplit.Name = "chkSplit";
+            this.chkSplit.Size = new System.Drawing.Size(125, 17);
+            this.chkSplit.TabIndex = 14;
+            this.chkSplit.Text = "Split every N minutes";
+            this.chkSplit.UseVisualStyleBackColor = true;
+            this.chkSplit.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // nudSplitInterval
+            // 
+            this.nudSplitInterval.Enabled = false;
+            this.nudSplitInterval.Location = new System.Drawing.Point(211, 169);
+            this.nudSplitInterval.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.nudSplitInterval.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudSplitInterval.Name = "nudSplitInterval";
+            this.nudSplitInterval.Size = new System.Drawing.Size(67, 20);
+            this.nudSplitInterval.TabIndex = 15;
+            this.nudSplitInterval.Value = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            // 
+            // nicon
+            // 
+            this.nicon.Icon = ((System.Drawing.Icon)(resources.GetObject("nicon.Icon")));
+            this.nicon.Text = "Timelapser by kasthack";
+            this.nicon.DoubleClick += new System.EventHandler(this.nicon_DoubleClick);
+            // 
             // frmMain
             // 
+            this.AcceptButton = this.btnGo;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(284, 244);
+            this.Controls.Add(this.nudSplitInterval);
+            this.Controls.Add(this.chkSplit);
+            this.Controls.Add(this.btnbrs);
             this.Controls.Add(this.nudFramerate);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.budBitrate);
@@ -221,13 +288,16 @@
             this.Controls.Add(this.lblPath);
             this.Controls.Add(this.txtPath);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "frmMain";
             this.Text = "Timelapser by kasthack";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.frmMain_SizeChanged);
             ((System.ComponentModel.ISupportInitialize)(this.nudFreq)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.budBitrate)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.nudFramerate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSplitInterval)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -249,6 +319,11 @@
         private System.Windows.Forms.NumericUpDown budBitrate;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.NumericUpDown nudFramerate;
+        private System.Windows.Forms.Button btnbrs;
+        private System.Windows.Forms.FolderBrowserDialog fbdSave;
+        private System.Windows.Forms.CheckBox chkSplit;
+        private System.Windows.Forms.NumericUpDown nudSplitInterval;
+        private System.Windows.Forms.NotifyIcon nicon;
     }
 }
 
--- source/TimeLapser/frmMain.cs
+++ source/TimeLapser/frmMain.cs
@@ -1,113 +1,200 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 using AForge.Video.FFMPEG;
+using Timer = System.Timers.Timer;
+
 namespace TimeLapser {
     public partial class frmMain : Form {
-        private bool recording = false;
+        private readonly Recorder _recorder = new Recorder();
+        private RecordSettings _settings;
 
-        public frmMain() {
-            InitializeComponent();
-        }
+        public frmMain() { InitializeComponent(); }
 
-        private async void btnGo_Click( object sender, EventArgs e ) {
-            recording ^= true;
-            if ( recording ) await Start();
-            else await Stop();
-        }
-
-        private void SetR( bool b ) {
-            btnGo.Text = b ? "Stop" : "Go";
-            lblTime.Text = !b ? "Pending" : "";
-        }
-
-        private async Task Stop() {
-            btnGo.Enabled = false;
-            await Task.Delay( 3000 );
-            btnGo.Enabled = true;
-        }
-
-        private async Task Start() {
-            SetR(true);
-            var outdir = txtPath.Text;
-            var format = (VideoCodec) cmbFormat.SelectedItem;
-            var delay = (int) nudFreq.Value;
-            var screenId = ( (ScreenInfo) cmbScreen.SelectedItem ).Rect;
-            var outfile = Path.Combine( outdir, DateTime.Now.ToFileTime() + ".avi" );
-            var bitrate = (int)budBitrate.Value * 1024;
-            var framerate = (int)nudFramerate.Value;
-            await this.Record(outfile, screenId, delay, framerate, format, bitrate);
-            SetR( false );
-        }
-
-        private async Task Record(string outfile, Rectangle rectangle = default( Rectangle ), int delay=500, int framerate = 30, VideoCodec format=VideoCodec.Default, int bitrate=24000) {
-            var sw = new Stopwatch();
-            sw.Start();
-            var b = rectangle!=default(Rectangle)?rectangle: Screen.AllScreens.OrderBy(a=>a.Bounds.X).First().Bounds;
-            int w = b.Width, h = b.Height, x = b.X, y = b.Y;
-            var sz = new Size( w, h );
-            using ( var outstream = new VideoFileWriter() ) {
-                var pd = Path.GetDirectoryName( outfile );
-                if ( !Directory.Exists( pd ) ) Directory.CreateDirectory( pd );
-                outstream.Open(outfile, w, h, framerate, format, bitrate);
-                using ( var bmp = new Bitmap( w, h ) ) {
-                    using ( var gr = Graphics.FromImage( bmp ) ) {
-                        while (recording)
-                        {
-                            lblTime.Text = sw.Elapsed.ToString("g");
-                            try {
-                                gr.CopyFromScreen( x, y, 0, 0, sz );
-                                preprocessFrame( gr );
-                                gr.Flush();
-                                outstream.WriteVideoFrame( bmp );
-                            }
-                            catch {}
-                            await Task.Delay( delay );
-                        }
-                    }
-                }
+        private void btnGo_Click( object sender, EventArgs e ) {
+            if ( _recorder.Recording ) {
+                _recorder.Stop();
+                SetR( false );
             }
-            sw.Stop();
+            else {
+                SetR(true);
+                _settings = new RecordSettings(
+                    outputPath: txtPath.Text,
+                    captureRectangle: ( (ScreenInfo)cmbScreen.SelectedItem ).Rect,
+                    fps: (int)nudFramerate.Value,
+                    interval: (int)nudFreq.Value,
+                    codec: (VideoCodec)cmbFormat.SelectedItem,
+                    bitrate: (int)budBitrate.Value << 20,
+                    splitInterval: chkSplit.Checked?(int?)(int)nudSplitInterval.Value :null,
+                    onFrameWritten: (a)=>BeginInvoke( (Action)(()=>lblTime.Text = string.Format( "Elapsed :{0}", a.ToString("g") )) )
+                );
+                _recorder.Start(
+                    _settings
+                );
+            }
         }
 
-        private void preprocessFrame( Graphics gr ) {
-            return;
+        private void SetR( bool recordRunning ) {
+            btnGo.Text = recordRunning ? "Stop" : "Go";
+            lblTime.Text = !recordRunning ? "Pending" : "";
+            txtPath.Enabled
+                = btnbrs.Enabled
+                = nudFreq.Enabled
+                = nudFramerate.Enabled
+                = cmbFormat.Enabled
+                = budBitrate.Enabled
+                = cmbScreen.Enabled
+                = chkSplit.Enabled
+                = nudSplitInterval.Enabled
+                = !recordRunning;
         }
-
         private void Form1_Load( object sender, EventArgs e ) {
-            cmbFormat.DataSource = (VideoCodec[]) Enum.GetValues( typeof( VideoCodec ) );
-            var scr = Screen.AllScreens.OrderBy( a=>a.Bounds.X ).ToArray();
-            
-            var screens = Enumerable
-                .Range(1, scr.Length)
-                .Select(a => new ScreenInfo { Id = a, Name = scr[a - 1].DeviceName, Rect = scr[a-1].Bounds})
-                .ToList();
-            var mx = scr.Min( a => a.Bounds.X );
-            var my = scr.Min( a => a.Bounds.Y );
-            var w = scr.Max( a => a.Bounds.Width + a.Bounds.X ) - mx;
-            var h = scr.Max(a => a.Bounds.Height + a.Bounds.Y) - my;
-            screens.Add( new ScreenInfo {
-                Id=screens.Count+1,
-                Name = "All screens",
-                Rect = new Rectangle( mx, my, w, h )
-            } );
-            cmbScreen.DataSource = screens;
+            cmbFormat.DataSource = Enum.GetValues( typeof( VideoCodec ) ) as VideoCodec[];
+            cmbScreen.DataSource = Recorder.GetScreenInfos();
             cmbFormat.SelectedIndex = 0;
             cmbScreen.SelectedIndex = cmbScreen.Items.Count - 1;
             txtPath.Text = Environment.GetFolderPath( Environment.SpecialFolder.MyVideos );
         }
+        private void btnbrs_Click( object sender, EventArgs e ) {
+            if ( fbdSave.ShowDialog() == DialogResult.OK ) txtPath.Text = fbdSave.SelectedPath;
+        }
+        private void checkBox1_CheckedChanged( object sender, EventArgs e ) => nudSplitInterval.Enabled = chkSplit.Checked;
+
+        private void nicon_DoubleClick( object sender, EventArgs e ) {
+            ShowInTaskbar = Visible = !( nicon.Visible = false);
+            BringToFront();
+            Activate();
+            Show();
+        }
+
+        private void frmMain_SizeChanged( object sender, EventArgs e ) {
+            if( WindowState == FormWindowState.Minimized ){
+                ShowInTaskbar = Visible = !( nicon.Visible = true);
+            }
+        }
     }
 
-    class ScreenInfo {
+    public class Recorder {
+        public bool Recording { get; private set; } = false;
+        private ManualResetEventSlim StopWaiter = new ManualResetEventSlim();
+        private Stopwatch _stopwatch;
+        public event EventHandler FrameWritten;
+
+        public void Start( RecordSettings settings ) {
+            var timer = new Timer();
+            if (Recording)
+                throw new InvalidOperationException("Recording is already started");
+            Recording = true;
+            new Thread( () => StartInternal(settings) ).Start();
+        }
+
+        private void StartInternal( RecordSettings settings ) {
+            try {
+                _stopwatch = _stopwatch ?? new Stopwatch();
+                _stopwatch.Reset();
+                _stopwatch.Start();
+                StopWaiter.Reset();
+                if ( !Directory.Exists( settings.OutputPath ) ) Directory.CreateDirectory( settings.OutputPath );
+                while ( Recording ) {
+                    var outfile = Path.Combine( settings.OutputPath, DateTime.Now.ToFileTime().ToString() + ".avi" );
+                    using (var outstream = new VideoFileWriter()) {
+                        var sourceRect = settings.CaptureRectangle;
+                        var w = sourceRect.Width;
+                        var h = sourceRect.Height;
+
+                        outstream.Open(
+                            outfile,
+                            w,
+                            h,
+                            settings.Fps,
+                            settings.Codec,
+                            settings.Bitrate );
+
+                        var mpf = settings.SplitInterval * 60000 / settings.Interval;
+                        using ( var bmp = new Bitmap( w, h ) ) {
+                            using ( var gr = Graphics.FromImage( bmp ) ) {
+                                for ( var i = 0; (mpf==null||i < mpf) && Recording; i++ ) {
+                                    try {
+                                        gr.CopyFromScreen( sourceRect.X, sourceRect.Y, 0, 0, sourceRect.Size );
+                                        PreprocessFrame( gr, settings );
+                                        gr.Flush();
+                                        outstream.WriteVideoFrame( bmp );
+                                        settings.OnFrameWritten?.Invoke( _stopwatch.Elapsed );
+                                    }
+                                    catch { }
+                                    Thread.Sleep( settings.Interval );
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch(Exception ex) {
+                //global
+            }
+            finally {
+                _stopwatch?.Stop();
+                StopWaiter.Set();
+            }
+        }
+
+        private void PreprocessFrame( Graphics gr, RecordSettings settings ) {
+            if ( !settings.Private ) return;
+            gr.Clear( Color.Black );
+            gr.Flush();
+        }
+
+        public void Stop() {
+            Recording = false;
+            StopWaiter.Wait();
+        }
+
+        public static IEnumerable<ScreenInfo> GetScreenInfos() {
+            var scr = Screen.AllScreens.OrderBy( a => a.Bounds.X ).ToArray();
+            var screens = Enumerable.Range( 1, scr.Length )
+                        .Select( a => new ScreenInfo { Id = a, Name = scr[ a - 1 ].DeviceName, Rect = scr[ a - 1 ].Bounds } )
+                        .ToList();
+            var mx = scr.Min( a => a.Bounds.X );
+            var my = scr.Min( a => a.Bounds.Y );
+            var w = scr.Max( a => a.Bounds.Width + a.Bounds.X ) - mx;
+            var h = scr.Max( a => a.Bounds.Height + a.Bounds.Y ) - my;
+            screens.Add( new ScreenInfo { Id = screens.Count + 1, Name = "All screens", Rect = new Rectangle( mx, my, w, h ) } );
+            return screens.ToArray();
+        }
+    }
+
+    public class RecordSettings {
+        public RecordSettings( string outputPath, Rectangle captureRectangle, int fps = 30, int interval = 500, VideoCodec codec = VideoCodec.MPEG4, int bitrate = 20, int? splitInterval = null, Action<TimeSpan> onFrameWritten = null ) {
+            OutputPath = outputPath;
+            CaptureRectangle = captureRectangle;
+            OnFrameWritten = onFrameWritten;
+            Interval = interval;
+            Fps = fps;
+            Codec = codec;
+            Bitrate = bitrate;
+            SplitInterval = splitInterval;
+            Private = false;
+        }
+        public int? SplitInterval { get; }
+        public int Bitrate { get; }
+        public VideoCodec Codec { get; }
+        public Rectangle CaptureRectangle { get; }
+        public int Interval { get; }
+        public int Fps { get; }
+        public string OutputPath { get; }
+        public bool Private { get; set; }
+        public Action<TimeSpan> OnFrameWritten { get; }
+    }
+
+    public class ScreenInfo {
         public Rectangle Rect;
         public int Id;
         public string Name;
-        public override string ToString() {
-            return string.Format( "{0}({1})", this.Name, this.Id );
-        }
+        public override string ToString() => string.Format( "{0}({1})", Name, Id );
     }
 }
