0000000014c4a1d40b7418963642023428c9ac11
Konstantin Safonov <kasthack@epicm.org>
2023-01-04 18:09:09 +0500

    Fix scaling issue, update publish profile and assembly version

--- src/TimeLapser/Program.cs
+++ src/TimeLapser/Program.cs
@@ -11,6 +11,7 @@
         [STAThread]
         private static void Main()
         {
+            Application.SetHighDpiMode(HighDpiMode.PerMonitorV2);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FrmMain());
